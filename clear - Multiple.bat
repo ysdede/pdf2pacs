0a17acbdc2c226ca6f5dd18d0508cf8d45f36a9a
ysdede <yunusseyhandede@gmail.com>
2019-07-16 22:51:13 +0300

    Batch files to clear cached test files

---
+++ clear - Multiple.bat
@@ -0,0 +1,2 @@
+clear.bat
+copy dumpPage*.dcm Temp\c255e9d518c1896b26115e696a6354bda163d486\*.*
---
+++ clear.bat
@@ -0,0 +1,4 @@
+del backlog\*.* /Q
+rmdir /S /Q Temp
+mkdir Temp
+REM copy sample.pdf rapor\sample.pdf
